55a103acd1d6e5d3a3318d5d8016a3ac25e2f388
jangjangji <sjjang41@naver.com>
2024-06-25 11:02:19 +0900

    기본 레이아웃구성

---
+++ src/main/java/org/choongang/member/controllers/MemberController.java
@@ -0,0 +1,31 @@
+package org.choongang.member.controllers;
+
+import org.choongang.global.config.annotations.Controller;
+import org.choongang.global.config.annotations.GetMapping;
+import org.choongang.global.config.annotations.PostMapping;
+import org.choongang.global.config.annotations.RequestMapping;
+
+@Controller
+@RequestMapping("/member")
+public class MemberController {
+    //회원가입 양식
+    @GetMapping("/join")
+    public String join(){
+        return "member/join";
+    }
+    // 회원 가입 처리
+    @PostMapping("/join")
+    public String joinPs(){
+        return null;
+    }
+    //로그인양식
+    @GetMapping("/login")
+    public String login(){
+        return "member/login";
+    }
+    @PostMapping("/login")
+    public String loginPs(){
+        return null;
+    }
+
+}
